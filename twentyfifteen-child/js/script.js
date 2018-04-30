464b7ca4e5716128a962d4932b02838c832a054f
LAPTOP-139HB6O3\Steven <stemarin10@gmail.com>
2018-04-30 17:54:39 -0500

    Adds js comments for the functions

--- twentyfifteen-child/js/script.js
+++ twentyfifteen-child/js/script.js
@@ -1,21 +1,36 @@
 (function() {
-	clearInterval();
-	
-
-	document.getElementById("cta_title").innerHTML = php_vars.title;
-	setDateCountdown();
+	clearInterval();//this clears the interval of the countdown
+	document.getElementById("cta_title").innerHTML = php_vars.title;//put the title, send in the params
+	setDateCountdown();//start the countdown
 })();
 
+/*
+======================================================
+This function starts the countdown
+======================================================
+*/
+
+
 function setDateCountdown(){
+	//start calculating the countdown date
 	var eventDate = new Date(php_vars.remaining);
 	var dateArray = getDateDifference(eventDate);
 	setHtmlDate(dateArray);
 	setInterval(
+		//each second send the new calculated date for the countdown
 		function(){
 			var dateArray = getDateDifference(eventDate);
 			setHtmlDate(dateArray);
 		}, 1000);
 }
+
+/*
+======================================================
+This function calculates the difference between a
+given date and now and returns and array with days, 
+hours, minutes and seconds.
+======================================================
+*/
 
 function getDateDifference(date_future){
 	var date_now = new Date();
@@ -50,12 +65,29 @@ function getDateDifference(date_future){
 
 }
 
+/*
+======================================================
+This function sets the date of the countdown, the
+parameters it receive is an array with days, hours, 
+minutes and seconds
+======================================================
+*/
+
+
+
 function setHtmlDate(dateArray){
 	document.getElementById("cta_countdown_days").innerHTML = dateArray.days;
 	document.getElementById("cta_countdown_hours").innerHTML = dateArray.hours;
 	document.getElementById("cta_countdown_mins").innerHTML = dateArray.minutes;
 	document.getElementById("cta_countdown_secs").innerHTML = dateArray.seconds;
 }
+
+/*
+======================================================
+This function adds the 0 at the start of the date if
+its less than 10
+======================================================
+*/
 
 function dateTwoDigits(n) {
   return (n < 10 ? '0' : '') + n;
